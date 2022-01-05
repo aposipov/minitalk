3f85d8c002ad09c4c3fe500938596b16b50c6201
aposipov <a.p.osipov@ro.ru>
2022-01-05 23:00:31 +1000

    ok

---
+++ inc/minitalk.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lchristi <lchristi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/05 16:41:21 by lchristi          #+#    #+#             */
+/*   Updated: 2022/01/05 16:59:13 by lchristi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include<signal.h>
+//# include<>
+//# include<>
+
+typedef struct s_data
+{
+	int		i;
+	char	c;
+	int		client_pid;
+}	t_data;
+
+//ft_atoi
+//ft_putstr
+//ft_putnbr
+//ft_putchar
+
+#endif
