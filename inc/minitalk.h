c404c4bef20a8e93b7918fd9cd4a487ad373b966
aposipov <a.p.osipov@ro.ru>
2022-01-06 22:34:38 +1000

    ok

--- inc/minitalk.h
+++ inc/minitalk.h
@@ -6,27 +6,27 @@
 /*   By: lchristi <lchristi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 16:41:21 by lchristi          #+#    #+#             */
-/*   Updated: 2022/01/05 16:59:13 by lchristi         ###   ########.fr       */
+/*   Updated: 2022/01/06 19:09:38 by lchristi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include<signal.h>
-//# include<>
+# include <signal.h>
+# include <unistd.h>
 //# include<>
 
-typedef struct s_data
-{
-	int		i;
-	char	c;
-	int		client_pid;
-}	t_data;
+//typedef struct s_data
+//{
+//	int		i;
+//	char	c;
+//	int		client_pid;
+//}	t_data;
 
 //ft_atoi
-//ft_putstr
-//ft_putnbr
-//ft_putchar
+void	ft_putstr(char *str);
+void	ft_putnbr(int n);
+void	ft_putchar(char c);
 
 #endif
