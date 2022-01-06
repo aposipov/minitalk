8230b05a7ce60ab4837e7c64106ec068f30bad1c
aposipov <a.p.osipov@ro.ru>
2022-01-06 22:34:10 +1000

    ok

--- src/ft_utils.c
+++ src/ft_utils.c
@@ -6,8 +6,34 @@
 /*   By: lchristi <lchristi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 17:00:33 by lchristi          #+#    #+#             */
-/*   Updated: 2022/01/05 17:00:36 by lchristi         ###   ########.fr       */
+/*   Updated: 2022/01/06 22:01:55 by lchristi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/minitalk.h"
+//#include <stdio.h>
+//#include <stdlib.h>
 
+void	ft_putstr(char *str)
+{
+	while(*str)
+	{
+		write(1, str++, 1);
+	}
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int n)
+{
+	if (n < 10)
+	{
+		ft_putchar(n + '0');
+		return ;
+	}
+	ft_putnbr(n / 10);
+	ft_putchar((n % 10) + '0');
+}
--- src/server.c
+++ src/server.c
@@ -6,8 +6,21 @@
 /*   By: lchristi <lchristi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 16:33:41 by lchristi          #+#    #+#             */
-/*   Updated: 2022/01/05 16:33:44 by lchristi         ###   ########.fr       */
+/*   Updated: 2022/01/06 19:03:06 by lchristi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/minitalk.h"
 
+int main()
+{
+	struct sigaction	sa;
+
+	//reset_data();
+	ft_putstr("pid: ");
+	ft_putnbr(getpid());
+	ft_putstr("\n");
+	sa.sa_flags = SA_SIGINFO;
+	//
+	
+}
