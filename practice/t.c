3f296ef3d2cde3f61bf6354586356018b5500250
aposipov <a.p.osipov@ro.ru>
2022-01-08 21:28:58 +1000

    ok

---
+++ practice/t.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int i = 1;
+
+	printf("argc = %d\n", argc - 1);
+	while(i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return(0);
+}
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: lchristi <lchristi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 16:33:41 by lchristi          #+#    #+#             */
-/*   Updated: 2022/01/07 18:45:32 by lchristi         ###   ########.fr       */
+/*   Updated: 2022/01/07 21:12:06 by lchristi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int main()
 	//struct sigaction	sa;
 
 	//reset_data();
-	ft_putstr("pid: ");
+	
+	ft_putstr("server is run\nPID: ");
 	ft_putnbr(getpid());
 	ft_putstr("\n");
 	//sa.sa_flags = SA_SIGINFO;
